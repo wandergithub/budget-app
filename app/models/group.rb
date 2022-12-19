aeab4a3cce35db1a7fe889e703e3c999289ed849
wandergithub <wandergonzalez0@hotmail.com>
2022-12-19 19:34:16 -0400

    Create group model and validations

---
+++ app/models/group.rb
@@ -0,0 +1,5 @@
+class Group < ApplicationRecord
+  belongs_to :user
+  
+  validates :name, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_225450) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_231304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon", default: "https://i.stack.imgur.com/Wttia.png"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +29,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_225450) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "groups", "users"
 end
