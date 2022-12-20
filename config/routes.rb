4ea68081c9feebd78589cc6110f83fb1600fead3
wandergithub <wandergonzalez0@hotmail.com>
2022-12-20 17:08:50 -0400

    add button-view and routes to create a new group

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'group#index'
 
-  resources :group, only: [:index]
+  resources :group, only: [:index, :new, :create]
   resources :home, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
