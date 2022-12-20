97ec9023a17234369b37db7b083c64b5f72760a3
wandergithub <wandergonzalez0@hotmail.com>
2022-12-20 10:47:02 -0400

    Add authentication with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,8 +10,11 @@ Rails.application.configure do
     Bullet.add_footer    = true
   end
 
-  # Settings specified here will take precedence over those in config/application.rb.
+  # Devise config
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+
+  # Settings specified here will take precedence over those in config/application.rb.
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
