687fa6301f3421c9ace39a8508b7d0f2454f9d6b
wandergithub <wandergonzalez0@hotmail.com>
2022-12-20 17:39:36 -0400

    Add #new #create to groupController

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -3,4 +3,27 @@ class GroupController < ApplicationController
   def index
 
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.new(group_params)
+    group.user = current_user
+
+    if group.save
+      flash[:message] = 'group saved!'
+      redirect_to group_index_path
+    else
+      flash[:message] = 'group could not be saved!'
+      redirect_to new_group_path
+    end
+  end
+
+  private
+  
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
