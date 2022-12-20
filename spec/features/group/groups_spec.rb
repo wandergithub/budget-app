98b0fc36879e70ae38ab8a349c7a24ab9c77e045
wandergithub <wandergonzalez0@hotmail.com>
2022-12-20 17:52:13 -0400

    Add groups#index display list of groups spec

--- spec/features/group/groups_spec.rb
+++ spec/features/group/groups_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Group pages integration examples', type: :feature do
+  # New user
   let(:user) { User.create(name: "example 1", password: "pasword11212", email: "email@exmaple.com") }
-  # Log in
+  
+  
   before(:each) do
+    # Log in
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
@@ -13,6 +16,17 @@ RSpec.describe 'Group pages integration examples', type: :feature do
   describe 'Group#index page functionality' do
     it 'Groups page is presented after logged in' do
       expect(page).to have_content("GROUPS")
+    end
+
+    it 'presents each group/categorie name and icon' do
+      # creates Groups
+    (0..5).each do |indx|
+      Group.create(name: "group #{indx}", user:)
+    end
+      Group.all.each do |group|
+        expect(page).to have_content(group.name)
+        expect(page).to have_content(group.icon)
+      end
     end
 
     it 'redirect to group#new form when add a new group button is clicked' do
