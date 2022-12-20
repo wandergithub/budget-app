45a4338e6d255278d515012841a7a05e8e90269a
wandergithub <wandergonzalez0@hotmail.com>
2022-12-20 17:38:54 -0400

    finish group#new form specs

--- spec/features/group/groups_spec.rb
+++ spec/features/group/groups_spec.rb
@@ -22,18 +22,21 @@ RSpec.describe 'Group pages integration examples', type: :feature do
   end
 
   describe 'Group#new page form functionality' do
+    before(:each) do
+      visit new_group_path
+    end
     it 'shows the new group form input fild' do
-      expect(page).to have_field("#group_name")
+      expect(page).to have_field("group_name")
     end
 
     it 'creates a new group' do
-      expect(Group.first).not_to exist
-      
+      expect(Group.first).to be_nil
+
       fill_in "group_name", with: "sample name"
-      fill_in "icon", with: "icon_link.png"
+      fill_in "group_icon", with: "icon_link.png"
       click_on "Save"
 
-      expect(Group.first).to exist
+      expect(Group.first).to be_an_instance_of(Group)
     end
   end
 end
