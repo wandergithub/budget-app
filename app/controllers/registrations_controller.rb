9db548d38ac2b3f90820a5dd815ad5c61dcfb2bb
wandergithub <wandergonzalez0@hotmail.com>
2023-02-18 15:31:49 -0400

    Change redirect link when users Signs up, now they are redirected to group#index

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
     protected
 
-    def after_sign_up_path_for()
+    def after_sign_up_path_for(resource)
         group_index_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
   root to: 'home#index'
 
   resources :group, only: [:index, :new, :create] do
     resources :expense, only: [:index, :new, :create]
   end
   resources :home, only: [:index]
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+ 
 end
