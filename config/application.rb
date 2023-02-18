21f97949347fcfdba4f99dfa8763ea0d3a987ed7
wandergithub <wandergonzalez0@hotmail.com>
2023-02-18 13:29:43 -0400

    Add Sass pre processor

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module BudgetApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Enable assets pipeline ---> for Sass
+    config.assets.enabled = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
