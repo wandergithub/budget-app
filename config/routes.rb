544045382ccb9402ca0b3180d5bac6cf868cab83
wandergithub <wandergonzalez0@hotmail.com>
2023-03-01 17:18:58 -0400

    Add Expense#update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :group, only: [:index, :new, :create, :destroy] do
-    resources :expense, only: [:index, :new, :create, :destroy]
+    resources :expense, only: [:index, :new, :create, :destroy, :update]
   end
   resources :home, only: [:index]
   
